9d6e4c164f87fb17f0252f088d82b802a8a1231d
joe <josulliv101@gmail.com>
2024-02-22 12:13:01 -0500

    update embla options

--- src/components/EmblaCarousel.tsx
+++ src/components/EmblaCarousel.tsx
@@ -19,27 +19,34 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
       <EmblaViewport options={options}>
         <div className="embla__container pl-4 pt-4">
           {profiles.map(({ id, name, pic }, index) => (
-            <Link key={id} href={`/profile/${id}`} prefetch={true}>
-              <div className="embla__slide w-[200px] h-[200px]">
-                {/* <img
+            <Link
+              className="embla__slide "
+              key={id}
+              href={`/profile/${id}`}
+              prefetch={true}
+            >
+              <div className="">
+                <div className="h-[200px]">
+                  {/* <img
                   className="embla__slide__img opacity-100 rounded-md"
                   src={pic}
                   alt={name}
                   width={"100%"}
                   height={"auto"}
                 /> */}
-                <Image
-                  className="embla__slide__img opacity-100 rounded-md border"
-                  src={pic}
-                  alt={name}
-                  width={200}
-                  height={200}
-                  priority={index < 4}
-                  // fill={true}
-                />
-              </div>
-              <div className="max-w-[200px] ml-4 text-sm text-center text-muted-foreground text-balance px-2 py-4">
-                {name}
+                  <Image
+                    className="embla__slide__img opacity-100 rounded-md border"
+                    src={pic}
+                    alt={name}
+                    width={200}
+                    height={200}
+                    priority={index < 4}
+                    // fill={true}
+                  />
+                </div>
+                <div className=" ml-4 text-sm text-center text-muted-foreground text-balance px-2 py-4">
+                  {name}
+                </div>
               </div>
             </Link>
           ))}
